f5fac446fe40d90d31f8fb5d2a801d51b084945f
Chen Li <498526554@qq.com>
2018-07-16 17:42:31 -0400

    scroll bar

--- app/src/main/java/com/example/c374li/fotagmobile/ImageCollectionView.java
+++ app/src/main/java/com/example/c374li/fotagmobile/ImageCollectionView.java
@@ -16,7 +16,7 @@ public class ImageCollectionView extends TableLayout implements Observer {
 
     ImageCollectionView(Context context, ImageCollectionModel imagecollectionmodel) {
         super(context);
-        View.inflate(context, R.layout.scroll, this);
+        View.inflate(context, R.layout.vertical_layout, this);
         Log.d(String.valueOf(R.string.DEBUG_FOTAG_ID), "ImageCollectionView: Constructor");
 
         this.imagecollectionmodel = imagecollectionmodel;
